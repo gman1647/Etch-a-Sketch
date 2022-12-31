90bab2d9b6746b6831035b2085d5cf8d33034d21
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2022-12-31 11:35:37 -0500

    Finish grid layout function.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,9 @@ let gridRows = document.createElement('div');
 const container = document.getElementById("container");
 let pixel = document.createElement('div')
 
-let width = 16;
+let width = 100;
+let pixelSize = (960/width) + "px";
+console.log(pixelSize)
 
 function addRows (rows){
     for (let i=0; i < (rows*rows); i++) {
@@ -11,6 +13,7 @@ function addRows (rows){
         gridRows.classList.add('gridRows');
         gridRows.setAttribute('id', i);
         container.appendChild(gridRows);
+        gridRows.style.width = pixelSize;
     }
     }
 
@@ -21,6 +24,7 @@ function addPixels (rows){
         pixel.classList.add('pixel');
         column = document.getElementById(i);
         column.appendChild(pixel);
+
     }
 }
 
