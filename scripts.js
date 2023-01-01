e35ea3761231a6a4b3c0c8325f626409ab91a4e8
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-01-01 15:17:48 -0500

    Add slider for input of pixels per side Added a function to take user input to select the numer of pixels per side and then redraw the grid. It takes a whlie for larger grid sizes around 100x100 pixels.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let gridRows = document.createElement('div');
 const container = document.getElementById("container");
 let pixel = document.createElement('div')
 
-let width = 35;
+let width = 10;
 let pixelSize = (500/width) + "px";
 console.log(pixelSize)
 
@@ -20,11 +20,28 @@ function addRows (rows){
     }
     }
 
-addRows(width)
-
+//addRows(width)
+/*
 function colorPixels (rows) {
     for (let i=0; i < (rows*rows); i++) {
         ids = 'pixel' + i;
         pixelSketched = document.getElementById(ids);pixelSketched.addEventListener
 
 }};
+*/
+let sliderWidth = document.getElementById('pixelSide')
+let output = document.getElementById('demo')
+output.textContent = sliderWidth.value;
+sliderWidth.oninput = function() {
+    for (let i = 0; i < (width*width); i++) {
+        ids = 'pixel' + i;
+        toRemove = document.getElementById(ids);
+        container.removeChild(toRemove);
+    }
+    output.textContent = this.value;
+    width = this.value;
+    pixelSize = (500/width) + "px";
+    addRows(width)
+}
+
+addRows(width)
