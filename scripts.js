bd1075f081a23fffadf30567df700623d9261148
Greg Baughman <75804705+gman1647@users.noreply.github.com>
2023-01-01 00:42:46 -0500

    Add sketch ability using mouseOver.

--- scripts.js
+++ scripts.js
@@ -2,31 +2,29 @@ let gridRows = document.createElement('div');
 const container = document.getElementById("container");
 let pixel = document.createElement('div')
 
-let width = 100;
-let pixelSize = (960/width) + "px";
+let width = 35;
+let pixelSize = (500/width) + "px";
 console.log(pixelSize)
 
 function addRows (rows){
     for (let i=0; i < (rows*rows); i++) {
-        console.log("appended row");
         let gridRows = document.createElement('div')
         gridRows.classList.add('gridRows');
-        gridRows.setAttribute('id', i);
+        ids = 'pixel' + i;
+        gridRows.setAttribute('id', (ids));
         container.appendChild(gridRows);
         gridRows.style.width = pixelSize;
+        gridRows.addEventListener('mouseenter', () => {
+        gridRows.style.backgroundColor = "#646665";
+        });
     }
     }
-
-function addPixels (rows){
-    for (let i=0; i < (rows); i++) {
-        console.log("appended pixel");
-        let pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        column = document.getElementById(i);
-        column.appendChild(pixel);
-
-    }
-}
 
 addRows(width)
 
+function colorPixels (rows) {
+    for (let i=0; i < (rows*rows); i++) {
+        ids = 'pixel' + i;
+        pixelSketched = document.getElementById(ids);pixelSketched.addEventListener
+
+}};
